f0be6e05ec8ef62fc84770a3627d36caf57ac46f
Duan2baka <duan2baka@gmail.com>
2023-04-17 16:30:31 +0800

    add details to the deck controller

--- Assets/Scripts/Controller/DeckController.cs
+++ Assets/Scripts/Controller/DeckController.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeckController : MonoBehaviour{
-    public GameObject emptyCard;
+    public List<GameObject> startList;
+    public emptyCard emptyCard;
+    public Card tmpCard;
+    public GameObject drawArea;
+    private List<Card> drawList;
+    private List<Card> discardList;
     void Start(){
-        
+        drawArea = GameObject.Find("DrawArea");
+        drawList = new List<Card>();
+        if(startList != null)
+            for(int i = 0; i < startList.Count; i ++)
+                drawList.Add(startList[i].GetComponent<Card>());
+        discardList = new List<Card>();
     }
     public Card peak(){
-        /// to be implemented
-        return emptyCard.GetComponent<Card>();
+        return drawList[0];
+    }
+    void deck_update(){
+        /// need to be further implemented
+        drawArea.transform.Find("RemainingCard").GetComponent<Text>().text = "" + drawList.Count;
     }
     public Card draw(){
-        /// to be implemented
-        return emptyCard.GetComponent<Card>();
+        if(drawList.Count == 0){ /// need to be further tested
+            discardList.Shuffle();
+            while(discardList.Count > 0){
+                drawList.Add(discardList[0]);
+                discardList.RemoveAt(0);
+            }
+        }
+        tmpCard = drawList[0];
+        drawList.RemoveAt(0);
+        discardList.Add(tmpCard);
+        deck_update();
+        return tmpCard;
     }
 }
