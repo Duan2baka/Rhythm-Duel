e43b9231d8339c5dda5571d38969d1c5ee871861
Duan2baka <duan2baka@gmail.com>
2023-04-30 12:23:47 +0800

    added visual effect to the break card

--- Assets/Scripts/Card/BreakCard.cs
+++ Assets/Scripts/Card/BreakCard.cs
@@ -8,6 +8,7 @@ public class BreakCard : MonoBehaviour, Card{
     private PlayerMovement playerMovement;
     private EnemyMovement enemyMovement;
     private FloorController floorController;
+    public GameObject effectPrefab;
     
     public void init(){
         playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
@@ -17,10 +18,29 @@ public class BreakCard : MonoBehaviour, Card{
     public void Cast(ref int currentMana){
         int x = playerMovement.getX(), y = playerMovement.getY();
         int ex = enemyMovement.getX(), ey = enemyMovement.getY();
-        if(y < 3)
-            floorController.get(x, y+1, true).GetComponent<FloorStatus>().takeDamage(100);
-        else
+        if(y < 3){
+            floorController.get(x, y + 1, true).GetComponent<FloorStatus>().takeDamage(100);
+            Vector3 position = floorController.getPositon(x, y + 1, true);
+            // Debug.Log(effectPrefab.GetComponent<Renderer>().bounds.size.y);
+            GameObject obj = Instantiate(effectPrefab, position + new Vector3(0f, effectPrefab.GetComponent<Renderer>().bounds.size.y / 2f, 0f), Quaternion.identity);
+            obj.layer = 1;
+            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
+            float scale = floorController.get(x, y + 1, true).GetComponent<Renderer>().bounds.size.x / size.x;
+            obj.transform.localScale = new Vector3(scale, scale, scale);
+            Destroy(obj, 0.3f);
+        }
+        else{
             floorController.get(x, 1, false).GetComponent<FloorStatus>().takeDamage(100);
+            Vector3 position = floorController.getPositon(x, 1, false);
+            // Debug.Log(effectPrefab.GetComponent<Renderer>().bounds.size.y);
+            GameObject obj = Instantiate(effectPrefab, position + new Vector3(0f, effectPrefab.GetComponent<Renderer>().bounds.size.y / 2f, 0f), Quaternion.identity);
+            obj.layer = 1;
+            Vector3 size = obj.GetComponent<Renderer>().bounds.size;
+            float scale = floorController.get(x, 1, false).GetComponent<Renderer>().bounds.size.x / size.x;
+            obj.transform.localScale = new Vector3(scale, scale, scale);
+
+            Destroy(obj, 0.3f);
+        }
         currentMana -= cost;
         return;
     }
